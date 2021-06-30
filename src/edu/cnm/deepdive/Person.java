36f3ed71a79251d63f259d38e3eb452bdf280630
Jan Gevaert <jan.gevaert@gmail.com>
2021-06-30 07:31:17 -0600

    Initial commit

---
+++ src/edu/cnm/deepdive/Person.java
@@ -0,0 +1,33 @@
+package edu.cnm.deepdive;
+
+public class Person {
+
+  private String firstName;
+  private String lastName;
+  private int age;
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  private String getLastName() {
+    return lastName;
+  }
+
+  private void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  private int getAge() {
+    return age;
+  }
+
+  private void setAge(int age) {
+    this.age = age;
+  }
+
+}
